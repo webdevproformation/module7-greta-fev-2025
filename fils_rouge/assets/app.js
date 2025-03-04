5259b81d1d51bf7466369d7bb5912d1bf585b1b8
Malik H <malik.h@webdevpro.net>
2025-03-04 15:30:56 +0100

    start menu back

--- fils_rouge/assets/app.js
+++ fils_rouge/assets/app.js
@@ -8,4 +8,4 @@ import './bootstrap.js';
 import './styles/app.css';
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
