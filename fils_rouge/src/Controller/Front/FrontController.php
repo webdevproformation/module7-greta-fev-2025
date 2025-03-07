c8a1c202c320f319c186555c3eee2d7af25ade2c
Malik H <malik.h@webdevpro.net>
2025-03-07 15:28:18 +0100

    mise en place d'un bouton de trie par date de création home

--- fils_rouge/src/Controller/Front/FrontController.php
+++ fils_rouge/src/Controller/Front/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Front;
 
 use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,11 +14,19 @@ final class FrontController extends AbstractController
     //https://127.0.0.1:8001/index.html.twig => NON !!! 
     #[Route('/', name: 'page_home')]
     public function index(
-        RecetteRepository $recetteRepository
+        RecetteRepository $recetteRepository,
+        Request $request 
     ): Response
     {
+        $trie = $request->query->get("trie", "desc");
+
+        if(!in_array($trie , ["asc", "desc"])){
+            $trie =  "desc";
+        }
+
         return $this->render('front/index.html.twig', [
-           "recettes" => $recetteRepository->findBy([] , ["dt_creation" => "DESC"])
+           "recettes" => $recetteRepository->findBy([] , ["dt_creation" => $trie]),
+           "trie" => $trie == "desc" ? "asc" : "desc"
         ]);
     }
 }
